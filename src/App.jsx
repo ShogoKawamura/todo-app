3a246ce3e125000274719ba366e5aa7b93a0ebe4
Shogo Kawamura <star.caitisith@gmail.com>
2022-06-21 05:31:49 +0900

    初期変更

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+export const App = () => {
+  return (
+  <>
+  <div>
+    <input placeholder="Todoを入力" />
+    <button>追加</button>
+  </div>
+  <div>
+    <p>未完了</p>
+    <ul>
+      <div>
+        <li>ああああ</li>
+        <button>完了</button>
+        <button>削除</button>
+      </div>
+      <div>
+        <li>いいいい</li>
+        <button>完了</button>
+        <button>削除</button>
+      </div>
+    </ul>
+  </div>
+  <div>
+    <p>未完了</p>
+    <ul>
+      <div>
+        <li>うううう</li>
+        <button>戻す</button>
+      </div>
+    </ul>
+  </div>
+  </>
+  );
+};
+
